c5c6a1a0a0a7dab82cbb1e442d59284d64876eab
thejassk7 <thejassuvarnak@gmail.com>
2026-02-05 18:29:33 +0530

    Added lists to reduce code lines

--- application.py
+++ application.py
@@ -47,6 +47,113 @@ ct_section=input("Enter Section: ")
 stud_name=input("Enter Student Name: ")
 stud_rollno=int(input("Enter Student Roll Number: "))
 no_subject=int(input("Enter number of Subjects: "))
+credit_subject=[]
+credit_subject_code=[]
+credit_subject_credit=[]
+credit_cie1=[]
+credit_cie2=[]
+credit_subject_lab_record=[]
+credit_assignment=[]
+credit_lab_exam=[]
+credit_exe=[]
+comp_subject=[]
+comp_subject_code=[]
+comp_cie1=[]
+comp_cie2=[]
+comp_assignment=[]
 i=0
-if i<n:
-    subject_credit=int(input(""))
+while i<no_subject:
+    subject=input("Enter the Subject: ")
+    subject_code=input("Enter Subject Code: ")
+    subject_credit=input("Enter Subject Credit: ")
+    if subject_credit>0:
+        credit_subject.append(subject)
+        credit_subject_code.append(subject_code)
+        credit_subject_credit.append(subject_credit)
+        marks=int(input("Enter CIE 1 Marks(Out of 45): "))
+        credit_cie1.append(marks)
+        marks=int(input("Enter CIE 2 Marks(Out of 45): "))
+        credit_cie2.append(marks)
+        marks=int(input("Enter Lab Record Marks(Out of 10): "))
+        credit_subject_lab_record.append(marks)
+        marks=int(input("Enter marks of assignment(Out of 10): "))
+        credit_assignment.append(marks)
+        marks=int(input("Enter Lab Exam Marks(Out of 15): "))
+        credit_lab_exam.append(marks)
+        marks=int(input("Enter SEE Marks(Out of 100): "))
+        credit_exe.append(marks)
+    else:
+        comp_subject.append(subject)
+        comp_subject_code.append(subject_code)
+        marks=int(input("Enter your CIE 1 Marks: (Out of 30) "))
+        comp_cie1.append(marks)
+        marks=int(input("Enter CIE 2 marks: (Out of 30) "))
+        comp_cie2.append(marks)
+        marks=int(input("Enter Assignment marks: (Out of 50) "))
+        comp_assignment.append(marks)
+print("---STUDENT'S PROFILE---")
+print("Name: ",stud_name)
+print("Roll Number: ",stud_rollno)
+print("Section: ",ct_section)
+print("Class Teacher: ",ct_name)
+print("PART-A: Credit Subjects")
+sum_credit=sum(credit_subject_credit)
+i=0
+credit_total_marks=[]
+backlog=[]
+cgpa=0
+while i<len(credit_subject):
+    print("Subject: ",credit_subject[i])
+    print("Subject Code: ",credit_subject_code[i])
+    print("Subject Credit: ",credit_subject_credit[i])
+    print("CIE 1 Marks: ",credit_cie1[i])
+    print("CIE 2 Marks: ",credit_cie2[i])
+    print("Assignment Marks: ",credit_assignment[i])
+    print("Lab Record Marks: ",credit_subject_lab_record[i])
+    print("Lab Exam: ",credit_lab_exam)
+    print("SEE Exam Marks: ",credit_exe[i])
+    marks=((credit_cie1[i]+credit_cie2[i])/6)+(credit_assignment[i])+(credit_lab_exam[i])+(credit_subject_lab_record[i])+((credit_exe)/2)
+    credit_total_marks.append(marks)
+    cgpa=cgpa+((marks*(credit_subject_credit[i]/sum_credit))*0.1)
+    print("Total Marks: ",credit_total_marks[i])
+    if credit_total_marks[i]<40:
+        print("Student Failed")
+        backlog.append(credit_subject[i])
+    else:
+        print("Student Passed")
+    i=i+1
+print("Part B: Compulsory Subjects")
+i=0
+while i<len(comp_subject):
+    print("Subject: ", comp_subject[i])
+    print("Subject Code: ",comp_subject_code[i])
+    print("CIE 1: ", comp_cie1[i])
+    print("CIE 2: ",comp_cie2[i])
+    print("Assignment: ",comp_assignment[i])
+    marks=((comp_cie1[i]+comp_cie2[i])/60)*50
+    ext_marks=marks+comp_assignment[i]
+    if ext_marks>40:
+        print("Your Student Passed")
+    else:
+        print("Your Student failed")
+    i=i+1
+if len(backlog)>0:
+    print("You have backlog in following subjects: ")
+    print(backlog)
+    print("Your CGPA: ",cgpa)
+else:
+    print("Congratulation. You do not have any backlog.")
+    print("Your CGPA: ",cgpa)
+
+    
+
+        
+    
+    
+
+
+
+
+
+
+    
